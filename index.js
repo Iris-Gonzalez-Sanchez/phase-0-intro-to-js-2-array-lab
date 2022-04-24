afb303baca4c8766b5e2a1c2f6b9e3b9051c2d42
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-04-24 05:36:24 -0400

    commit tree ab6e8b1ca385153cb62ff7ef2eae7e98ec7b8a1c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+    const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const lastCat = [...cats, name];
+    return lastCat;
+}
+
+function prependCat(name) {
+    const firstCat = [name, ...cats];
+    return firstCat;
+}
+
+function removeLastCat() {
+    const byeLastCat = cats.slice(0, cats.length-1);
+    return byeLastCat;
+}
+
+function removeFirstCat() {
+    const byeFirstCat = cats.slice(1);
+    return byeFirstCat;    
+}
